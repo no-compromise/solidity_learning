3a26bb10140abb87091accb915cfd3e7f2aa2317
no-compromise <4andrej@gmail.com>
2022-07-24 20:50:54 +0200

    testing in brownie

--- brownie/scripts/deploy.py
+++ brownie/scripts/deploy.py
@@ -1,9 +1,23 @@
-from brownie import accounts
+from turtle import update
+from brownie import accounts, config, SimpleStorage
+import os
 
 
 def deploy_simple_storage():
     account = accounts[0]
-    print(account)
+    # account = accounts.add(config["wallets"]["from_key"])
+    # print(account)
+
+    simple_storage = SimpleStorage.deploy({"from": account})
+    print(simple_storage)
+
+    simple_value = simple_storage.retrieve()
+    print(simple_value)
+
+    tranasaction = simple_storage.store(15, {"from": account})
+    tranasaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
 
 
 def main():
